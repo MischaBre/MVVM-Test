9058400ff85308e76c905da587529fb4f406c55e
MischaGe <gemsa.michael@gmail.com>
2021-10-21 21:31:40 +0200

    Initial Commit

---
+++ MVVM-Test/MVVM_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_TestApp.swift
+//  MVVM-Test
+//
+//  Created by Michael Gemsa on 21.10.21.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
